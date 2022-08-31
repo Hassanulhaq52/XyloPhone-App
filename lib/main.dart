4ce313be6fdfc6c61663933ed7f59411e5d6bbab
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-08-31 14:24:40 +0500

    Assets added.

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+
+          child: Container(
+
+          ),
+        ),
+      ),
+    );
+  }
+}
