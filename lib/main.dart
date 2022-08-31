dd976fe312d4236fd5d8ff4ad9b5467c8e02e454
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-08-31 21:29:20 +0500

    Refactoring Our App.

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,14 @@ class XylophoneApp extends StatelessWidget {
     player.play(AssetSource("note$soundNumber.wav"));
   }
 
-  buildKey() {
-    Expanded(
+  Expanded buildKey({Color color, int soundNumber}) {
+    return Expanded(
       child: TextButton(
         style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(Colors.red),
+          backgroundColor: MaterialStateProperty.all(color),
         ),
         onPressed: () {
-          playSound(1);
+          playSound(soundNumber);
         },
       ),
     );
@@ -31,14 +31,14 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              buildKey();
-              buildKey();
-              buildKey();
-              buildKey();
-    buildKey();
-    buildKey();
-    buildKey();
-              ],
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
           ),
         ),
       ),
