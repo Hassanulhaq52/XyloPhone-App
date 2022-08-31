f825ddfc0a0492be57981a3dd975c8f0c508944f
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-08-31 20:35:54 +0500

    Updating the UI of Our App.

--- lib/main.dart
+++ lib/main.dart
@@ -4,80 +4,42 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  void playSound(int soundNumber){
+  void playSound(int soundNumber) {
     final player = AudioPlayer();
     player.play(AssetSource("note$soundNumber.wav"));
   }
+
+  buildKey() {
+    Expanded(
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.red),
+        ),
+        onPressed: () {
+          playSound(1);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white70,
         body: SafeArea(
-          child: Column(children: [
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.red),
-              ),
-              onPressed: ()  {
-        playSound(1);
-              },
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(Colors.orange.shade700),
-              ),
-              onPressed: ()  {
-                playSound(2);
-              },
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(Colors.yellow.shade600),
-              ),
-              onPressed: ()  {
-                playSound(3);
-              },
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(Colors.green.shade700),
-              ),
-              onPressed: ()  {
-                playSound(4);
-              },
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(Colors.teal.shade800),
-              ),
-              onPressed: ()  {
-                playSound(5);
-              },
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(Colors.blue.shade700),
-              ),
-              onPressed: ()  {
-                playSound(6);
-              },
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(Colors.purple.shade700),
-              ),
-              onPressed: ()  {
-                playSound(7);
-              },
-            ),
-          ]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey();
+              buildKey();
+              buildKey();
+              buildKey();
+    buildKey();
+    buildKey();
+    buildKey();
+              ],
+          ),
         ),
       ),
     );
